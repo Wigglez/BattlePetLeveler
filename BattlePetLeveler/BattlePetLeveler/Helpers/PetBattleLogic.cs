0c4c248d172cc797972ea624e4ef549893a50b0b
Wigglez <lod_damageinc@hotmail.com>
2013-11-07 10:37:43 -0600

    pet battle logic

---
+++ BattlePetLeveler/BattlePetLeveler/Helpers/PetBattleLogic.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BattlePetLeveler.Helpers {
+    public class PetBattleLogic {
+
+        // ===========================================================
+        // Constants
+        // ===========================================================
+
+        // ===========================================================
+        // Fields
+        // ===========================================================
+
+        // ===========================================================
+        // Constructors
+        // ===========================================================
+
+        // ===========================================================
+        // Getter & Setter
+        // ===========================================================
+
+        // ===========================================================
+        // Methods for/from SuperClass/Interfaces
+        // ===========================================================
+
+        // ===========================================================
+        // Methods
+        // ===========================================================
+
+        public static void UseRandomAbility() {
+            var randNum = RandomNumber.GenerateRandomInt(1, 3);
+
+            var getCanUseAbility = PetBattles.GetAbilityState(1, 1, randNum);
+
+            if (getCanUseAbility[0] == "0")
+            {
+                PriorityTreeState.UsedRandomAbility = false;
+            }
+            else
+            {
+                BattlePetLeveler.BPLLog("used " + randNum);
+                PriorityTreeState.UsedRandomAbility = true;
+                PetBattles.UseAbility(randNum);
+            }
+
+            PriorityTreeState.UsedRandomAbility = true;
+        }
+
+        // ===========================================================
+        // Inner and Anonymous Classes
+        // ===========================================================
+
+    }
+}
